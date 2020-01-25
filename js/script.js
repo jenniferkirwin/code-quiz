d02d1fcfeb2fd365d1831d7100cf87046af13e11
jenniferkirwin <lamontagneartist@gmail.com>
2020-01-25 11:22:38 -0500

    Functioning quiz. Need to add to local storage.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+// setting variables
+
 let questions = [
   {
     "quest" : "What is JavaScript's official name?",
@@ -24,63 +26,94 @@ let questions = [
 ];
 
 let quizDiv = $("#quiz");
-let timer = 60;
+let timerDisp = $(".timer");
+let highScores = $(".highScores");
 let arrayIndex = 0;
-var t;
+let score = 60;
+let finalScore;
+let t;
+let userHighScores = JSON.parse(localStorage.getItem("userHighScores"));
 
-function startQuiz() {
+console.log(userHighScores);
 
-};
+// Start the Quiz
 
-// https://jsfiddle.net/Daniel_Hug/pvk6p/
+$(document).on("click", ".btn-start", function() {
+  $(".btn-large").addClass("disabled");
+  arrayIndex = 0;
+  timer();
+  renderQuestion();
+});
+
+// Timer Function
 
 function countDown() {
-  t = setTimeout(function() {
-    timer--;
-    removeSec();
-  }, 1000);
+    score--;
+    timer();
+    timerDisp.text(score);
 }
 
-function removeSec() {
-  timer--;
-};
-
-countDown();
-console.log(timer);
-
-renderQuestion();
+function timer() {
+    t = setTimeout(countDown, 1000);
+      if (score < 1 || arrayIndex > questions.length - 1) {
+    clearTimeout(t);
+  }
+}
 
 // shows a question to the user
 function renderQuestion() {
 
+  if (arrayIndex < questions.length) {
+      
     quizDiv.append($("<p>").text(questions[arrayIndex].quest));
 
     for (let j = 0; j < questions[arrayIndex].choice.length; j++) {
       quizDiv.append($("<button>").text(questions[arrayIndex].choice[j]).addClass("btn-choice btn-large mycolor"));
     };
+  }
+
+  else {
+    endGame();
+  };
 };
 
 // takes the question, checks if correct, increments to next question
 
 $(document).on("click", ".btn-choice", function() {
   let choice = $(this).text();
-  console.log(choice);
   if (choice === questions[arrayIndex].choice[3]) {
     // trigger toast message
-    // alert("correct!");
     M.toast({html: 'Correct!'});
   }
 
   else {
     // trigger loosing toast message
-    timer--;
+    score = score - 10;
+    $(".card").addClass("shake");
     M.toast({html: `Oops, the correct answer is ${questions[arrayIndex].choice[3]}`});
+          setTimeout(function() {
+        $(".card").removeClass("shake");
+      }, 800);
   };
   arrayIndex++;
   quizDiv.empty();
   renderQuestion();
 });
 
+// End of Game
+
+function endGame() {
+  clearTimeout(t);
+  let elem = document.querySelector('.modal');
+  let instance = M.Modal.init(elem);
+  M.Toast.dismissAll();
+  finalScore = score;
+  $(".finalScore").text(finalScore);
+  score = 60;
+  timerDisp.text(score);
+  instance.open();
+  $(".btn-large").removeClass("disabled");
+};
 
 
     // randomize/shuffle basePassword array
@@ -101,5 +134,35 @@ $(document).on("click", ".btn-choice", function() {
     //   return array;
     // };
  
+// Create object to store score in local storage
+
+$("#submit").on("click", function() {
+  let userName = $("#name").val();
+
+  // if (userHighScores !== null) {
+  //   userHighScores[userHighScores.length] = {
+  //     username: userName.trim(),
+  //     score: finalScore
+  //   };
+  // }
+
+  // else {
+  //   userHighScores[0] = {
+  //     username: userName.trim(),
+  //     score: finalScore
+  //   };
+  // };
+
+  // retrieve JSON object
+  // append new element to JSON object
+  // set new 
+  // https://stackoverflow.com/questions/736590/add-new-attribute-element-to-json-object-using-javascript
+  
+
+  console.log(userHighScores[0]);
+});
+
+// Display the latest user high scores
+
 
 });
